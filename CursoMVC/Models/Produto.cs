c48e1576b25d0b7b55e035412d8cca65a573caa1
Raphael Simoes Andrade <73614828+raphaelsa16603@users.noreply.github.com>
2021-04-06 18:36:02 -0300

    Ajustes e uso do DataAnnotations

--- CursoMVC/Models/Produto.cs
+++ CursoMVC/Models/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,11 @@ namespace CursoMVC.Models {
     public class Produto {
         public int Id { get; set; }
 
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "O campo descrição é obrigatório.")]
         public string Descricao{ get; set; }
 
+        [Range(0, 400, ErrorMessage = "Valor fora da faixa, de 0 até 400.")]
         public int  Quantidade { get; set; }
 
         public int CategoriaId { get; set; }
