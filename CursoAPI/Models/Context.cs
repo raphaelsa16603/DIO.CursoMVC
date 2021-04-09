0b8faac2a8b3b8539960469e80905caaef750bd3
Raphael Simoes Andrade <73614828+raphaelsa16603@users.noreply.github.com>
2021-04-09 19:32:55 -0300

    Novo projeto

---
+++ CursoAPI/Models/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoAPI.Models {
+    public class Context : DbContext{
+        protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseSqlite(@"Data Source = ..\CursoMVC\CursoAspMVC.db");
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
---
+++ CursoAPI/Models/Produto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoAPI.Models {
+    public class Produto {
+        public int Id { get; set; }
+
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "O campo descrição é obrigatório.")]
+        public string Descricao{ get; set; }
+
+        [Range(0, 400, ErrorMessage = "Valor fora da faixa, de 0 até 400.")]
+        public int  Quantidade { get; set; }
+
+        public int CategoriaId { get; set; }
+
+        public Categoria SuaCategoria { get; set; }
+
+    }
+}
